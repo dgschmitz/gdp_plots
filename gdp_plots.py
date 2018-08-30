783d38a83a140f6526c0a8b82cf28ec6f8ebbdd1
Dylan Schmitz <dgschmitz@wisc.edu>
2018-08-30 14:24:55 -0500

    Changed to read in many different files and plot all

--- gdp_plots.py
+++ gdp_plots.py
@@ -7,25 +7,26 @@ import pandas
 import matplotlib.pyplot as plt
 
 #parse inputs, filename should be at index 1
-filename = sys.argv[1];
+file_list = sys.argv[1:]
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
 
 # read data into a pandas dataframe and transpose
 #filename = 'gapminder_gdp_oceania.csv'
-data = pandas.read_csv(filename, index_col = 'country').T
+for filename in file_list:
+    data = pandas.read_csv(filename, index_col = 'country').T
 
-# create a plot the transposed data
-ax = data.plot(title=filename)
+    # create a plot the transposed data
+    ax = data.plot(title=filename)
 
-#axes labels
-ax.set_xlabel('Year')
-ax.set_ylabel('GDP Per Capita')
+    #axes labels
+    ax.set_xlabel('Year')
+    ax.set_ylabel('GDP Per Capita')
 
-#set axes ticks
-ax.set_xticks(range(len(data.index)))
-ax.set_xticklabels(data.index, rotation=45)
+    #set axes ticks
+    ax.set_xticks(range(len(data.index)))
+    ax.set_xticklabels(data.index, rotation=45)
 
-# display the plot
-plt.show()
+    # display the plot
+    plt.show()
